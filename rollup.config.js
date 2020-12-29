ab5aef5d00d7df7aa0c01eb80da7a99192ada7b7
Jocelyn Badgley <jbadgley@clarocity.com>
2020-12-29 13:42:12 -0800

    Switching to spaces from tabs

--- rollup.config.js
+++ rollup.config.js
@@ -5,77 +5,77 @@ import banner from 'rollup-plugin-banner';
 import { join } from 'path';
 
 const bannerConfig = {
-	file: join(__dirname, 'LICENSE.txt'),
+  file: join(__dirname, 'LICENSE.txt'),
 };
 
 export default [
 
-	{
-		input: 'src/utils.js',
-		output: {
-			file: 'dist/utils.js',
-			format: 'cjs',
-			exports: 'named',
-		},
-		plugins: [
-			resolve(),
-			banner(bannerConfig),
-		],
-	},
+  {
+    input: 'src/utils.js',
+    output: {
+      file: 'dist/utils.cjs.js',
+      format: 'cjs',
+      exports: 'named',
+    },
+    plugins: [
+      resolve(),
+      banner(bannerConfig),
+    ],
+  },
 
-	{
-		input: 'src/utils.js',
-		output: {
-			file: 'dist/utils.esm.js',
-			format: 'esm',
-		},
-		plugins: [
-			resolve(),
-			banner(bannerConfig),
-		],
-	},
-	{
-		input: 'src/utils.js',
-		output: {
-			file: 'dist/utils.esm.min.js',
-			format: 'esm',
-		},
-		plugins: [
-			resolve(),
-			terser({
-				output: {
-					comments: false,
-				},
-				compress: {
-					ecma: 2018,
-					keep_classnames: true,
-					module: true,
-				},
-			}),
-			banner(bannerConfig),
-		],
-	},
+  {
+    input: 'src/utils.js',
+    output: {
+      file: 'dist/utils.esm.js',
+      format: 'esm',
+    },
+    plugins: [
+      resolve(),
+      banner(bannerConfig),
+    ],
+  },
+  {
+    input: 'src/utils.js',
+    output: {
+      file: 'dist/utils.esm.min.js',
+      format: 'esm',
+    },
+    plugins: [
+      resolve(),
+      terser({
+        output: {
+          comments: false,
+        },
+        compress: {
+          ecma: 2018,
+          keep_classnames: true,
+          module: true,
+        },
+      }),
+      banner(bannerConfig),
+    ],
+  },
 
-	{
-		input: 'src/utils.js',
-		output: {
-			file: 'dist/utils.browser.js',
-			format: 'umd',
-			exports: 'named',
-			name: 'TwippedUtils',
-		},
-		plugins: [
-			resolve(),
-			babel({
-				exclude: 'node_modules/**',
-				presets: [
-					'@babel/env',
-				],
-			}),
-			terser({ output: {
-				comments: false,
-			} }),
-			banner(bannerConfig),
-		],
-	},
+  {
+    input: 'src/utils.js',
+    output: {
+      file: 'dist/utils.browser.js',
+      format: 'umd',
+      exports: 'named',
+      name: 'TwippedUtils',
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**',
+        presets: [
+          '@babel/env',
+        ],
+      }),
+      terser({ output: {
+        comments: false,
+      } }),
+      banner(bannerConfig),
+    ],
+  },
 ];
