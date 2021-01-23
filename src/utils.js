b2ec109a024d1c266ddc97852090b9dc5ac5972e
Jocelyn Badgley (Twipped) <joc@twipped.com>
2021-01-22 22:08:24 -0800

    Make eslint shutup about requestAnimationFrame

--- src/utils.js
+++ src/utils.js
@@ -1350,8 +1350,8 @@ export function jsonSoftParse (input, fallback) {
 
 export function defer (fn) {
   if (typeof cancelAnimationFrame === 'undefined') return timeout(fn, 0);
-  const handle = requestAnimationFrame(fn);
-  return () => cancelAnimationFrame(handle);
+  const handle = requestAnimationFrame(fn); // eslint-disable-line no-undef
+  return () => cancelAnimationFrame(handle); // eslint-disable-line no-undef
 }
 
 export function timeout (fn, time) {
