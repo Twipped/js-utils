68986954996116af14b091adfc70490b69d4ba95
Jocelyn Badgley (Twipped) <joc@twipped.com>
2021-01-23 19:30:02 -0800

    Added tests for iterator and promise functions

---
+++ tests/iterators.js
@@ -0,0 +1,52 @@
+
+import tap from 'tap';
+import { mapMode, isIterator, isIterable, isGenerator, ensureIterable, MAPMODE, map } from '../src/index';
+
+const array = [ 1, 2, 3 ];
+const object = { a: 1, b: 2, c: 3 };
+const generator = function* () {
+  let i = 3;
+  while (i) yield i--;
+};
+
+const emptyIterator = { next: () => ({ done: true }) };
+const emptyIterable = { [Symbol.iterator]: () => emptyIterator };
+
+tap.equal(mapMode(array), MAPMODE.ARRAY);
+tap.equal(mapMode(generator), false);
+tap.equal(mapMode(generator()), MAPMODE.ITERATOR);
+tap.equal(mapMode(emptyIterator), MAPMODE.ITERATOR);
+tap.equal(mapMode(emptyIterable), MAPMODE.ITERABLE);
+tap.equal(mapMode(object), MAPMODE.OBJECT);
+
+tap.equal(isIterator(emptyIterable), false);
+tap.equal(isIterator(emptyIterator), true);
+tap.equal(isIterator(array), false);
+tap.equal(isIterator(generator), false);
+tap.equal(isIterator(generator()), true);
+tap.equal(isIterator(object), false);
+
+tap.equal(isIterable(emptyIterable), true);
+tap.equal(isIterable(emptyIterator), false);
+tap.equal(isIterable(array), true);
+tap.equal(isIterable(generator), false);
+tap.equal(isIterable(generator()), true);
+tap.equal(isIterable(object), false);
+
+tap.equal(isGenerator(emptyIterable), false);
+tap.equal(isGenerator(emptyIterator), false);
+tap.equal(isGenerator(array), false);
+tap.equal(isGenerator(generator), true);
+tap.equal(isGenerator(generator()), false);
+tap.equal(isGenerator(object), false);
+
+tap.ok(isIterable(ensureIterable(emptyIterator)));
+tap.ok(isIterable(ensureIterable(emptyIterable)));
+tap.ok(isIterator(ensureIterable(emptyIterator)));
+tap.notOk(isIterator(ensureIterable(emptyIterable)));
+tap.notOk(isGenerator(ensureIterable(emptyIterator)));
+
+const mappedGenerator = map(generator());
+tap.type(mappedGenerator, Array);
+tap.same(mappedGenerator, [ 3, 2, 1 ]);
+
---
+++ tests/promises.js
@@ -0,0 +1,46 @@
+
+import tap from 'tap';
+import { pmap, pdelay, prace, preduce } from '../src/index';
+
+const array = [ 100, 110, 70, 10, 11, 150, 220, 250 ];
+
+tap.test('promise delay', async (t) => {
+  const now = Date.now();
+  await pdelay(500)();
+  t.ok(Date.now() - now > 500, 'It took more than half a second to resolve');
+});
+
+tap.test('promise race', async (t) => {
+
+  const result = await prace(
+    pdelay(100)(100),
+    pdelay(110)(110),
+    pdelay(70)(70),
+    pdelay(10)(10),
+    pdelay(11)(11),
+  );
+
+  t.equal(result, 10, 'The fastest promise resolved');
+});
+
+
+tap.test('promise map no concurrency', async (t) => {
+
+  const result = await pmap(array, (v) => pdelay(v)(v));
+
+  t.same(result, array, 'Non concurrent results.');
+});
+
+tap.test('promise map with concurrency', async (t) => {
+
+  const result = await pmap(array, (v) => pdelay(v)(v), { concurrency: 3 });
+
+  t.same(result, array, 'Concurrent results.');
+});
+
+tap.test('promise reduce', async (t) => {
+
+  const result = await preduce(array, (prev, v) => pdelay(v)(prev + v), 0);
+
+  t.same(result, 921, 'Reduced result.');
+});
