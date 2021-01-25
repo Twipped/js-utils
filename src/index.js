68a87f60070f8161ac9bbd0a84b806628b552cb6
Jocelyn Badgley (Twipped) <joc@twipped.com>
2021-01-25 11:25:22 -0800

    Fixed bug with slugify

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export * from './any';
 export * from './anyBy';
 export * from './anyOf';
 export * from './promises';
-export * from './slugify';
+export { default as slugify } from './slugify';
 export * from './assert';
 export * from './is';
 export * from './isArrayOf';
