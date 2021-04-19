45036ae79bc9431d924c0acf0b96c18974ecfab1
Jocelyn Badgley <jbadgley@clarocity.com>
2021-04-19 10:41:25 -0700

    Added PURE statements to the isArrayOf and contains creations so they get tree shook

--- src/isArrayOf.js
+++ src/isArrayOf.js
@@ -24,17 +24,17 @@ export function isArrayOf (...args) {
     return true;
   };
 }
-export const isArrayofArrays =     isArrayOf(isArray);
-export const isArrayOfStrings =    isArrayOf(isString);
-export const isArrayOfNumbers =    isArrayOf(isNumber);
-export const isArrayOfBooleans =   isArrayOf(isBoolean);
-export const isArrayOfObjects =    isArrayOf(isObject);
-export const isArrayOfMappables =  isArrayOf(isMappable);
-export const isArrayOfPrimatives = isArrayOf(isPrimitive);
-export const isArrayOfFunctions =  isArrayOf(isFunction);
-export const isArrayOfRegEx =      isArrayOf(isRegExp);
-export const isArrayOfTruthy =     isArrayOf(isTruthy);
-export const isArrayOfFalsey =     isArrayOf(isFalsey);
+export const isArrayOfArrays =     /* #__PURE__*/isArrayOf(isArray);
+export const isArrayOfStrings =    /* #__PURE__*/isArrayOf(isString);
+export const isArrayOfNumbers =    /* #__PURE__*/isArrayOf(isNumber);
+export const isArrayOfBooleans =   /* #__PURE__*/isArrayOf(isBoolean);
+export const isArrayOfObjects =    /* #__PURE__*/isArrayOf(isObject);
+export const isArrayOfMappables =  /* #__PURE__*/isArrayOf(isMappable);
+export const isArrayOfPrimatives = /* #__PURE__*/isArrayOf(isPrimitive);
+export const isArrayOfFunctions =  /* #__PURE__*/isArrayOf(isFunction);
+export const isArrayOfRegEx =      /* #__PURE__*/isArrayOf(isRegExp);
+export const isArrayOfTruthy =     /* #__PURE__*/isArrayOf(isTruthy);
+export const isArrayOfFalsey =     /* #__PURE__*/isArrayOf(isFalsey);
 
 export function contains (...args) {
   const predicate = is(...args);
@@ -46,15 +46,15 @@ export function contains (...args) {
     return false;
   };
 }
-export const containsArrays =     contains(isArray);
-export const containsStrings =    contains(isString);
-export const containsNumbers =    contains(isNumber);
-export const containsBooleans =   contains(isBoolean);
-export const containsObjects =    contains(isObject);
-export const containsMappables =  contains(isMappable);
-export const containsPrimatives = contains(isPrimitive);
-export const containsFunctions =  contains(isFunction);
-export const containsRegEx =      contains(isRegExp);
-export const containsTruthy =     contains(isTruthy);
-export const containsFalsey =     contains(isFalsey);
+export const containsArrays =     /* #__PURE__*/contains(isArray);
+export const containsStrings =    /* #__PURE__*/contains(isString);
+export const containsNumbers =    /* #__PURE__*/contains(isNumber);
+export const containsBooleans =   /* #__PURE__*/contains(isBoolean);
+export const containsObjects =    /* #__PURE__*/contains(isObject);
+export const containsMappables =  /* #__PURE__*/contains(isMappable);
+export const containsPrimatives = /* #__PURE__*/contains(isPrimitive);
+export const containsFunctions =  /* #__PURE__*/contains(isFunction);
+export const containsRegEx =      /* #__PURE__*/contains(isRegExp);
+export const containsTruthy =     /* #__PURE__*/contains(isTruthy);
+export const containsFalsey =     /* #__PURE__*/contains(isFalsey);
 
