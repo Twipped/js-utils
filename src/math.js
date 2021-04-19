f271e3734912055ee5d1fd56037b2d0e543378b4
Jocelyn Badgley <jbadgley@clarocity.com>
2021-04-19 10:43:03 -0700

    Added median average function

--- src/math.js
+++ src/math.js
@@ -34,6 +34,19 @@ export function avg (...collection) {
   return sums / collection.length;
 }
 
+export function median (...collection) {
+  if (!collection.length) return 0;
+  collection = flatten(collection);
+
+  if (collection.length % 2) {
+    const i = Math.floor(collection.length / 2);
+    return collection[i];
+  }
+
+  const i = Math.floor(collection.length / 2);
+  return (Number(collection[i]) + (collection[i + 1])) / 2;
+}
+
 export function stddev (...collection) {
   if (!collection.length) return 0;
   collection = flatten(collection);
