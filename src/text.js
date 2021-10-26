183fd2da5d69a1a96f50b8ccd6afe3e2ab59d800
Jocelyn Badgley <jbadgley@voxturvaluation.com>
2021-10-25 17:31:23 -0700

    Added a text similarity function that’s good for filtering and sorting.

--- src/text.js
+++ src/text.js
@@ -75,3 +75,28 @@ export function jsonSoftParse (input, fallback) {
     return fallback;
   }
 }
+
+export function similarity (str1, str2) {
+  function get_bigrams (string) {
+    var s = string.toLowerCase();
+    var v = s.split('');
+    for (var i = 0; i < v.length; i++) {
+      v[i] = s.slice(i, i + 2);
+    }
+    return v;
+  }
+
+  if (str1.length && str2.length) {
+    var pairs1 = get_bigrams(str1);
+    var pairs2 = get_bigrams(str2);
+    var union = pairs1.length + pairs2.length;
+    var hits = 0;
+    for (var x = 0; x < pairs1.length; x++) {
+      for (var y = 0; y < pairs2.length; y++) {
+        if (pairs1[x] === pairs2[y]) hits++;
+      }
+    }
+    if (hits > 0) return ((2.0 * hits) / union);
+  }
+  return 0.0;
+}
