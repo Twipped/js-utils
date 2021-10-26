e46dff27c4fdd902105cc758f72b4c7147f36508
Jocelyn Badgley <jbadgley@voxturvaluation.com>
2021-10-25 17:30:23 -0700

    Added `mapper`, a utility for converting object schemas

--- src/objects.js
+++ src/objects.js
@@ -135,3 +135,36 @@ export function set (obj, path, value) {
 
   return obj;
 }
+
+
+export function mapper (map) {
+  map = map.map(([ target, origin, transform ]) => {
+    if (typeof target === 'function') return target;
+
+    if (origin === null || origin === true || origin === false) {
+      return (input, output) => { set(output, target, origin); };
+    }
+    if (typeof origin === 'function') {
+      if (typeof transform === 'function') {
+        return (input, output, ...args) => { set(output, target, transform(origin(input, ...args))); };
+      }
+      return (input, output, ...args) => { set(output, target, origin(input, ...args)); };
+    }
+    if (typeof origin === 'string') {
+      if (typeof transform === 'function') {
+        return (input, output, ...args) => { set(output, target, transform(get(input, origin), ...args)); };
+      }
+      return (input, output) => { set(output, target, get(input, origin)); };
+    }
+    return (input, output) => { set(output, target, get(input, target)); };
+  });
+
+
+  return (source, ...args) => {
+    const result = {};
+    for (const fn of map) {
+      fn(source, result, ...args);
+    }
+    return result;
+  };
+}
