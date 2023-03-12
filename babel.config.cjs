0c2e1c40347b1d81e3a42be324a9acd87743e5c8
Jocelyn Badgley (Twipped) <joc@twipped.com>
2023-03-11 17:41:01 -0800

    feat: Now exports both commonjs and esm builds, with type definitions

---
+++ babel.config.cjs
@@ -0,0 +1,18 @@
+module.exports = exports = {
+  presets: [
+    [
+      '@babel/preset-env', {
+        useBuiltIns: 'entry',
+        corejs: 3,
+        exclude: [ 'transform-typeof-symbol' ],
+        modules: process.env.ESM ? false : 'cjs',
+        targets: 'maintained node versions',
+      },
+    ],
+  ],
+  plugins: process.env.NODE_ENV === 'test'
+    ? []
+    : [
+      [ 'babel-plugin-add-import-extension', { extension: process.env.ESM ? 'mjs' : 'cjs' } ],
+    ],
+};
